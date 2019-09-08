888913c899abdad3bca86a5caa62ae205e592a66
Jetug <ksergeev01@gmail.com>
2019-09-08 20:34:26 +0300

    Begin

---
+++ Chess/Models/Figure.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.Models
+{
+    abstract class Figure
+    {
+        public abstract string Icon { get; }
+    }
+}
---
+++ Chess/ViewModels/MainViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess.ViewModels
+{
+    class MainViewModel: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnProperteyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
